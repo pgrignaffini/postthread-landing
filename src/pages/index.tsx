9a14ecfcaa7e434f40d75a7b4f47b384e5b86cb6
grignaffini <grignaffinipaolo@gmail.com>
2023-01-28 17:16:35 +0100

    fixed display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,18 +13,18 @@ const Home: NextPage = () => {
       </Head>
       <main className="min-h-screen bg-gradient-radial from-[#2D2C2A] to-[#120D16] ">
         <div className="grid grid-cols-2 ">
-          <div className="col-span-1 flex flex-col items-center  space-y-12 p-4">
-            <div className="mt-16 flex items-center space-x-4">
+          <div className="col-span-1 flex flex-col items-center justify-center  space-y-12 p-4">
+            <div className="flex items-center space-x-4">
               <img src="/logo.svg" alt="PostThread Logo" className="w-64" />
-              <p className="font-poppins mt-4 text-4xl text-white">
+              <p className="font-poppins mt-4 text-4xl text-slate-200">
                 on Mastodon
               </p>
             </div>
             <div className="mx-auto w-3/4 space-y-4 text-justify">
-              <h3 className="text-3xl font-bold text-primary">
+              <h3 className="text-3xl font-bold text-violet-300">
                 Social Media that Rewards Instead of Extracts
               </h3>
-              <h5 className="text-xl">
+              <h5 className="text-xl text-slate-200">
                 PostThread gives social media users control over their feed,
                 data and privacy. We compensate content creators in crypto for
                 the value they provide.
@@ -34,14 +34,14 @@ const Home: NextPage = () => {
               <button className="btn-primary btn">Go to PostThread.xyz</button>
             </Link>
             <div className="w-3/4 space-y-8 ">
-              <p className="text-justify text-xl italic">
+              <p className="text-justify text-xl italic text-slate-200">
                 PostThread is a Decentralized Social Media Application powered
                 by Mastodon connected to the Polkadot Parachain Frequency.
                 Please visit our{" "}
                 <Link
                   target="_blank"
                   href="https://mcian91.gitbook.io/postthread/"
-                  className="text-primary underline"
+                  className="text-violet-500 underline"
                 >
                   white paper
                 </Link>{" "}
@@ -49,7 +49,7 @@ const Home: NextPage = () => {
                 philosophy.{" "}
               </p>
             </div>
-            <Info />
+            {/* <Info /> */}
           </div>
           <div className="col-span-1">
             <img
